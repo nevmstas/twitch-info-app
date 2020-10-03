070049b17dcea7046052b3bcf17c70b24c8024dc
Stas <nevmyvakastas@gmail.com>
2020-10-03 23:16:16 +0600

    created api service

--- src/redux/actions.ts
+++ src/redux/actions.ts
@@ -4,6 +4,10 @@ import { ThunkAction } from "redux-thunk";
 
 import { FETCH_GAMES, SHOW_LOADER, HIDE_LOADER } from "./types";
 
+import ApiService from "../services/api";
+
+const apiService = new ApiService();
+
 export const fetchGames = (): ThunkAction<
   void,
   RootState,
@@ -11,18 +15,7 @@ export const fetchGames = (): ThunkAction<
   Action<string>
 > => async (dispatch) => {
   dispatch({ type: SHOW_LOADER });
-  const response = await fetch(
-    "https://api.twitch.tv/kraken/games/top?limit=100",
-    {
-      headers: {
-        Accept: "application/vnd.twitchtv.v5+json",
-        "Client-ID": "lb30znza29uxvo1sgjgzmpgik2lbeo",
-      },
-    }
-  );
-
-  const json = await response.json();
-  console.log(json);
+  const json = await apiService.getData("games/top?limit=100");
   dispatch({ type: FETCH_GAMES, payload: json.top });
   dispatch({ type: HIDE_LOADER });
 };
---
+++ src/services/api.js
@@ -0,0 +1,19 @@
+export default class ApiService {
+  _devEndpoint = "https://api.twitch.tv/kraken/";
+
+  //games/top?limit=100
+  getData = async (body) => {
+    try {
+      const response = await fetch(`${this._devEndpoint}${body}`, {
+        headers: {
+          Accept: "application/vnd.twitchtv.v5+json",
+          "Client-ID": "lb30znza29uxvo1sgjgzmpgik2lbeo",
+        },
+      });
+      const json = await response.json();
+      return json;
+    } catch (e) {
+      return e.response;
+    }
+  };
+}
